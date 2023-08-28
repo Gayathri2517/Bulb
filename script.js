4b712f37b5d9fc99ddee5bed496ee936e7f636ae
Gayathri2517 <143204951+Gayathri2517@users.noreply.github.com>
2023-08-28 09:29:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var bulbon=document.getElementById("bon");
+var bulboff=document.getElementById("boff");
+var image=document.getElementById("bulb");
+bulbon.addEventListener("click",function() {
+    image.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGM2RROvj5W7gziZk7M5GJ6g93Ukj8gHFYSw&usqp=CAU";
+});
+bulboff.addEventListener("click",function(){
+    image.src="https://images.freeimages.com/images/previews/70a/light-bulb-1-1427502.jpg";
+});
